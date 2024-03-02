27519ecf9da655401996d706c3fce18f99976cd9
Frcsora <150892201+Frcsora@users.noreply.github.com>
2024-03-02 19:32:10 +0100

    Create Kyu6_NotSoRandom.java

---
+++ Kyu6_NotSoRandom.java
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/58ad2e9c0e3c08126000003f
+class Kata {
+  public static String notSoRandom(int b, int w) {
+    if(b % 2 != 0){
+      return "Black";
+    }
+    return "White";
+  }
+}
