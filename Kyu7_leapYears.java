d8fe07a95a5eef66d4e220c514a5a66d9228b779
Frcsora <150892201+Frcsora@users.noreply.github.com>
2024-03-02 18:44:54 +0100

    Create Kyu7_leapYears.java

---
+++ Kyu7_leapYears.java
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/526c7363236867513f0005ca
+public class LeapYears {
+
+  public static boolean isLeapYear(int year) {
+    if(year % 400 == 0){
+      return true;
+    }else if(year % 100 == 0){
+      return false;
+    } else if(year % 4 == 0){
+      return true;
+    }
+    return false;
+  }
+}
