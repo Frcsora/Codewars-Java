fb92e64e49a49f2160acceb5e2e224f70c5f00fc
Frcsora <150892201+Frcsora@users.noreply.github.com>
2024-03-02 20:07:19 +0100

    Create Kyu8_ReturnNegative.java

---
+++ Kyu8_ReturnNegative.java
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/55685cd7ad70877c23000102
+public class Kata {
+
+  public static int makeNegative(final int x) {
+    int i = x;
+    if(i < 0){
+      return i;
+    }
+    return i * -1; // Your code here.
+    
+  }
+  
+}
