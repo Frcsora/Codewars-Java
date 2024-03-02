5c1f4b1f41a30d50b02ac8faa6d4264cace69545
Frcsora <150892201+Frcsora@users.noreply.github.com>
2024-03-02 20:51:49 +0100

    Create Kyu7_StringEndWith.java

---
+++ Kyu7_StringEndWith.java
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+public class Kata {
+  public static boolean solution(String str, String ending) {
+    return str.endsWith(ending);
+  }
+}
