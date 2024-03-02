ce74dd38471a7d3094c39f82402878923203fef0
Frcsora <150892201+Frcsora@users.noreply.github.com>
2024-03-02 19:12:04 +0100

    Create Kyu5_NotVerySecure.java

---
+++ Kyu5_NotVerySecure.java
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/526dbd6c8c0eb53254000110
+public class SecureTester{
+  
+  public static boolean alphanumeric(String s){
+    return s.matches("[A-Za-z\\d]+");
+  }
+
+}
